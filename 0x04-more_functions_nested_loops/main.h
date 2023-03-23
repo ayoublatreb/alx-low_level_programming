049e11b27c928ec2d64a414fba8a5b9683ce354a
ayoublatreb <ayoublatreb@gmail.com>
2023-03-23 14:03:33 +0100

    0-putchar.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,16 +15,10 @@
  * print_to_98 - prints all natural numbers to 98
  * print_times_table - print times table up to
  */
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int);
-int _isalpha(int);
-int print_sign(int);
-int _abs(int);
-int print_last_digit(int);
-int add(int, int);
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int);
-void print_times_table(int n);
+int _isupper(int c);
+
+#endif /* MAIN_H */
+
