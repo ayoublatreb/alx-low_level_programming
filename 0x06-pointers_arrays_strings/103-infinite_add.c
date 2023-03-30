4669fece4438916debde96697931f8d6d084ae0c
ayoublatreb <ayoublatreb@gmail.com>
2023-03-30 15:13:19 +0200

    11-12

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,58 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * infinite_add - Adds two numbers stored as strings
+ * @n1: First number
+ * @n2: Second number
+ * @r: Buffer to store result
+ * @size_r: Size of buffer r
+ *
+ * Return: Pointer to the result, or 0 if the result can't be stored in r
  */
-
-void rev_string(char *n)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+    int len1, len2, max_len, i, j, sum, carry = 0;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
-	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
-		i--;
-	}
-	if (digits == size_r)
-		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
+    len1 = strlen(n1);
+    len2 = strlen(n2);
+
+    max_len = len1 > len2 ? len1 : len2;
+
+    if (max_len + 1 > size_r)
+        return 0;
+
+    r[max_len + 1] = '\0';
+
+    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
+    {
+        sum = carry;
+        if (i >= 0)
+            sum += n1[i] - '0';
+        if (j >= 0)
+            sum += n2[j] - '0';
+
+        if (sum >= 10)
+        {
+            carry = 1;
+            sum -= 10;
+        }
+        else
+        {
+            carry = 0;
+        }
+
+        r[max_len--] = sum + '0';
+    }
+
+    if (carry)
+    {
+        r[max_len] = carry + '0';
+        return r;
+    }
+
+    return r + 1;
 }
+
